8e85913bd7cba61a2328d311edd4b3dc5964e356
Jerwin Cruz <cruz.jerwin15@gmail.com>
2026-05-06 03:29:23 +0800

    Initial commit

--- config/db.js
+++ config/db.js
@@ -3,13 +3,13 @@ require('dotenv').config();
 
 
 const pool = mysql.createPool({
-   host: process.env.MYSQLHOST,
-  port: process.env.MYSQLPORT,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  waitForConnections: true,
-  connectionLimit: 10,
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_NAME,
+    waitForConnections:true,
+    connectionLimit:10,
+    queueLimit:0
 });
 
 module.exports =pool;
